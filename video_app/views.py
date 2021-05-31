cc5829546993856ae0ddc15c4b8396acf40b3d2e
Timi Olusegun <timiddon97@gmail.com>
2021-05-31 12:07:34 +0100

    feat: add sign in view

--- video_app/views.py
+++ video_app/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "video_app/index.html")
+
+def sign_in(request):
+    
+    return NotImplemented()
