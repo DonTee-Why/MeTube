64011bc94c4f748b420fa1232f62df3fdf128206
Timi Olusegun <timiddon97@gmail.com>
2021-05-31 12:07:11 +0100

    feat: add sign in path

--- video_app/urls.py
+++ video_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = "video_app"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("signIn", views.sign_in, name="signIn")
 ]
