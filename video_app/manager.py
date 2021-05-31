24cf6ec333e8bae6ea0458f0d276374f27b85677
Timi Olusegun <timiddon97@gmail.com>
2021-05-31 11:50:25 +0100

    ref: put comment in proper location

--- video_app/manager.py
+++ video_app/manager.py
@@ -21,13 +21,13 @@ class UserManager(BaseUserManager):
         return user
 
     def create_user(self, email, password=None, **extra_fields):
-        """
-        Create and save a SuperUser with the given email and password.
-        """
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
