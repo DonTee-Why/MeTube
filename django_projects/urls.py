c71db50bcc0d59a25c63acd93378b45b306ecc3f
Timi Olusegun <timiddon97@gmail.com>
2021-06-04 14:13:45 +0100

    fix: remove references to video_app and add meTube

--- django_projects/urls.py
+++ django_projects/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('meTube/', include("video_app.urls"))
+    # path('meTube/', include("video_app.urls")),
+    path('meTube/', include("meTube.urls"))
 ]
