139c4111f9027438cceffcae75394ccc59010f4a
Timi Olusegun <timiddon97@gmail.com>
2021-06-04 13:39:15 +0100

    feat: remove video_app app

--- video_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "video_app"
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("signIn", views.sign_in, name="signIn")
-]
